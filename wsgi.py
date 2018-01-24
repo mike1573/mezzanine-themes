032cf5519bb8a6429fd83f7990d1139a1eba0252
mike1573 <mike1573@hotmail.com>
2018-01-24 16:18:40 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 import os
+from django.core.wsgi import get_wsgi_application #
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
