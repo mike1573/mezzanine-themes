2dafe7442219293b7a63ecec3c5043c59b0fa8b1
mike1573 <mike1573@hotmail.com>
2018-01-24 20:26:18 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,9 +6,9 @@ from django.core.wsgi import get_wsgi_application #
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "%s.settings" % real_project_name("{{ project_name }}"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+#                      "%s.settings" % real_project_name("{{ project_name }}"))
 
 
 #from django.core.wsgi import get_wsgi_application
